1786fce42f1e456962cbf663dc1d1658f5d71855
kaser137 <107580630+kaser137@users.noreply.github.com>
2023-04-20 06:06:32 +0300

    update

--- warehouse/main.py
+++ warehouse/main.py
@@ -1,14 +1,16 @@
 import os
 import dotenv
-# import asyncio
+import datetime
+import asyncio
+import time
 import funcs
 from emoji import emojize
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher, FSMContext
+from aiogram import Bot, types, Dispatcher
+from aiogram.dispatcher import FSMContext
 from aiogram.utils import executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import StatesGroup, State
-from asgiref.sync import sync_to_async
+from asgiref.sync import sync_to_async, async_to_sync
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,11 +20,13 @@ owner_id = os.environ['OWNER_ID']
 bot = Bot(token=token)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
+today = datetime.date.today()
 
 
 class UserState(StatesGroup):
     mail = State()
     phone = State()
+    storage = State()
 
 
 # start division____________________________________________________________
@@ -75,18 +79,81 @@ async def accept_registration(msg: types.Message):
 
 
 @dp.message_handler(lambda msg: not msg.text[0] == '/', state=UserState.mail)
-async def accept_registration(msg: types.Message, state: FSMContext):
+async def registrate_client(msg: types.Message, state: FSMContext):
     chat_id = msg.from_user.id
     tg_account = msg.from_user.username
     mail = msg.text
     await sync_to_async(funcs.registration_client)(tg_account, chat_id, mail)
     await bot.send_message(owner_id, f'new client has been registered,\nchat_id: {chat_id}\ntg_account: {tg_account}')
-    await msg.answer('You have been registered')
+    await msg.answer('You have been registered\n for list storages type /storages')
     await state.finish()
 
 
 @dp.message_handler(lambda msg: msg.text[0] == '/', state=UserState.mail)
-async def accept_registration(msg: types.Message):
+async def catch_invalid_eail(msg: types.Message):
     await msg.answer('Incorrect email, repeat input')
 
-executor.start_polling(dp)
+
+@dp.message_handler(commands=['storages'])
+async def output_list_sorages(msg: types.Message):
+    storages = await sync_to_async(funcs.get_available_storages)()
+    for storage in storages:
+        await msg.answer(
+            f'№ storage: {storage.id}\naddress storage: {storage.address}\navailable area: {await sync_to_async(storage.free_space)()}\n===========')
+    await msg.answer('for make new order type № of storage\nfor cancel type /cancel')
+    await UserState.storage.set()
+
+
+@dp.message_handler(lambda msg: msg.text[0] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+                    state=UserState.storage)
+async def choose_storage(msg: types.Message, state: FSMContext):
+    await msg.answer('all is good')
+    await state.finish()
+
+
+@dp.message_handler(lambda msg: msg.text[0] not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+                    state=UserState.storage)
+async def catch_invalid_storage(msg: types.Message):
+    await msg.answer('Incorrect №, repeat input')
+
+
+async def p():
+    await asyncio.sleep(3)
+    print('5')
+
+
+async def sentinel():
+    while 1:
+        await asyncio.sleep(10)
+        orders = await sync_to_async(funcs.get_expired_orders)()
+        print(orders)
+        for order in orders:
+            print(order)
+            await bot.send_message(order['chat_id'],
+                                   f'expired order: {order["order"]},\nclient: {order["client"]}\nstorage: {order["storage"]}\n'
+                                   f'expired days: {order["expired days"]}\n===========')
+            await bot.send_message(owner_id,
+                                   f'expired order: {order["order"]},\nclient: {order["client"]}\nstorage: {order["storage"]}\n'
+                                   f'expired days: {order["expired days"]}\n===========')
+
+
+async def sentinel3():
+    while 1:
+        await asyncio.sleep(7)
+        orders = await sync_to_async(funcs.get_terms_orders3)()
+        print(3333333)
+        for order in orders:
+            print(3)
+            await bot.send_message(order['chat_id'],
+                                   f'expired order: {order["order"]},\nclient: {order["client"]}\nstorage: {order["storage"]}\n'
+                                   f'expired days: {order["expired days"]}\n===========')
+            await bot.send_message(owner_id,
+                                   f'expired order: {order["order"]},\nclient: {order["client"]}\nstorage: {order["storage"]}\n'
+                                   f'expired days: {order["expired days"]}\n===========')
+
+
+async def on_startup(_):
+    asyncio.gather(sentinel(), sentinel3())
+
+
+executor.start_polling(dp, skip_updates=False, on_startup=on_startup)
